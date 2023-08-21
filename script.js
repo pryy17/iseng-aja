cdf081cdb1009b8b5b269f7a9009f8b26d02c2a0
pryy17 <priandy170501@gmail.com>
2023-08-21 10:47:28 +0700

    apa saja

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let pacar = prompt("pacar kamu siapa?", "chifuyu");
+let pacar = prompt("pacar kamu siapa?", "kaiser");
 
 function pacarSiapa(pacarku) {
     let pacars = pacarku;
     if( pacar == "priandy") {
         alert(" love u sayangku :3 ")
     } else {
-        alert("oooo gaitu yaudahh")
+        alert("parah si kalo g bisa jawab")
         pacar = prompt("pacar kamu siapa serius");
         pacarSiapa(pacar);
     }
